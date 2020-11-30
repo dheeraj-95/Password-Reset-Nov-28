9f260661388166a933328af540c952a0ffda55b2
dheeraj <gumudavellidheeraj@gmail.com>
2020-11-30 23:00:33 +0530

    Added env file to test in heroku

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
+const port = process.env.PORT || 8080;
+app
+    .use(cors())
+    .use(cookieParser())
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({extended : true}))
+    .use(express.static("public"))
+    .get('/',(req,res) => {
+        res.json({status : process.env.SECRET})
+    })
+    .listen(port)
